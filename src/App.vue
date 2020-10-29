fe4821ec92e2c11718e26db240eeee245532f961
coke <18220828184@163.com>
2020-10-29 16:01:46 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -15,15 +15,15 @@
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   }
    /*改变菜单展开颜色*/
-  .el-submenu .el-menu-item{
+  .sideBar .el-submenu .el-menu-item{
     background-color: #1f2d3d!important;
   }
   /*划过菜单变色*/
-  .el-submenu .el-menu-item:hover{
+  .sideBar .el-submenu .el-menu-item:hover{
     background-color: #001528!important;
   }
   /*菜单收缩样式*/
-  .el-menu--collapse{
+  .sideBar .el-menu--collapse{
     height: 100%;
     overflow-y: scroll;
   }
